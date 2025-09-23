3da1ed6b2ce30da39666855971fc1ded19caa99b
manishkHecco <manish@hecco.ai>
2025-09-23 14:51:04 +0530

    serverless

--- server/lambda.js
+++ server/lambda.js
@@ -1,27 +1,5 @@
 // lambda.js
-import { createServer, proxy } from "@vendia/serverless-express";
+import serverless from "serverless-http";
 import app from "./server.js";
-import { connectDB } from "./utils/dbconnect.js";
-let server;
-let initPromise;
 
-async function ensureServer() {
-  if (server) return server;
-  if (!initPromise) {
-    initPromise = (async () => {
-      // connect to DB once per container
-      await connectDB();
-      // create the vendia server wrapper so Express gets proper stream-like req/res
-      server = createServer(app);
-      return server;
-    })();
-  }
-  return initPromise;
-}
-
-export const handler = async (event, context) => {
-  // ensure initialization (DB + server wrapper) completed
-  await ensureServer();
-  // proxy the API Gateway event to the wrapped Express server
-  return proxy(server, event, context);
-};
+export const handler = serverless(app);
